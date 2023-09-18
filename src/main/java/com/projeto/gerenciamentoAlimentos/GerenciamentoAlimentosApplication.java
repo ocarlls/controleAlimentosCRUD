756332ec001c06f8caccd894b40dc625c6540712
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-09-18 20:42:28 -0300

    Commit inicial

---
+++ src/main/java/com/projeto/gerenciamentoAlimentos/GerenciamentoAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciamentoAlimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAlimentosApplication.class, args);
+	}
+
+}
