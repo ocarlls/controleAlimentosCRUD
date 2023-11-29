b1cae0086db2a39298975deb1a0a016c23df16c7
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-11-29 14:04:44 -0300

    feat: Adicionando função que disponibiliza autocomplete ao buscar um nome de um alimento

--- src/main/java/com/projeto/gerenciamentoAlimentos/domain/food/Alimento.java
+++ src/main/java/com/projeto/gerenciamentoAlimentos/domain/food/Alimento.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Entity
+@Entity(name = "Alimentos")
 @Table(name = "alimentos")
 public class Alimento implements Serializable {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
